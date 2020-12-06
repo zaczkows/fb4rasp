a68a5fec06f20cd0379aec95b6f5d4e369df5232
Piotr Zaczkowski <pzaczkowski@protonmail.com>
2020-12-06 23:53:06 +0100

    Improvements, part 1:
    - use cargo-make (really cool)
    - display CPU usage
    - random text movement in y

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,36 @@
 use fb4rasp;
+use rand::distributions::Distribution;
+use sysinfo::{ProcessorExt, SystemExt};
 
 async fn draw_time() {
     let mut fb = fb4rasp::Fb4Rasp::new().unwrap();
     let mut interval = tokio::time::interval(tokio::time::Duration::from_millis(1000));
-    let mut x: i32 = 10;
+    let mut x: i32;
     let mut x_diff = 1;
-    let mut y: i32 = 100;
+    let mut y: i32;
     let mut y_diff = 1;
 
     fb.init_events();
 
+    let dist_uni = rand::distributions::Uniform::from(0..20);
+    let mut rng = rand::thread_rng();
+    let mut system = sysinfo::System::new_all();
+
+    // First we update all information of our system struct.
+    system.refresh_all();
+
     loop {
+        system.refresh_cpu();
+        system.refresh_memory();
+
+        x = 0;
+        y = 80 + dist_uni.sample(&mut rng);
         fb.clean();
         fb.start();
         let local_time = chrono::Local::now();
         fb.set_color(&fb4rasp::Color {
-            red: x as f64 / 480.0,
-            green: y as f64 / 320.0,
+            red: 0.0,
+            green: 1.0,
             blue: 0.0,
             alpha: 1.0,
         });
@@ -26,18 +40,42 @@ async fn draw_time() {
                 x: x as f64,
                 y: y as f64,
             },
-            local_time.format("%d.%m.%Y %H:%M:%S").to_string().as_str(),
+            local_time
+                .format("%a, %d.%m.%Y, %H:%M:%S")
+                .to_string()
+                .as_str(),
         );
+        y = y + 26;
+
+        let (cpu_avg_usage, cpu_info) = {
+            let processors = system.get_processors();
+            let count = processors.len();
+            let mut ci = String::new();
+            let mut avg: f32 = 0.0;
+            let mut separator = "";
+            for p in processors {
+                ci.push_str(&format!("{}{:.0}", separator, p.get_cpu_usage()));
+                separator = ", ";
+                avg += p.get_cpu_usage();
+            }
+            (avg / count as f32, ci)
+        };
+
         fb.set_font_size(26.0);
         fb.render_text(
             &fb4rasp::Point {
                 x: x as f64,
-                y: (y + 26) as f64,
+                y: y as f64,
             },
-            format!("CPU Temp: {:.1}°C", fb4rasp::get_cpu_temperature())
-                .to_string()
-                .as_str(),
+            &format!(
+                "CPU: {:.0}% [{}] ({:.1}°C)",
+                cpu_avg_usage,
+                &cpu_info,
+                fb4rasp::get_cpu_temperature()
+            ),
         );
+        y = y + 24;
+
         x = x + x_diff;
         y = y + y_diff;
         if x > 50 || x < 1 {
